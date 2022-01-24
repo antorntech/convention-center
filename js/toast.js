9d21133ec92db3cd58640de69f73e2636ea55abf
antor10655gh <creativefreelancerantor@gmail.com>
2022-01-24 16:29:11 +0600

    add toast on nav-bar

---
+++ js/toast.js
@@ -0,0 +1,12 @@
+var toastTrigger = document.getElementById('liveToastBtn')
+var toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', function () {
+    var toast = new bootstrap.Toast(toastLiveExample)
+
+    toast.show()
+  })
+}
+
+
+
